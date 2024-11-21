fe129778df980d94bbae86dc9f294007944ca30c
OlgaSm33 <olya.sudakova98@gmail.com>
2024-11-21 21:36:08 +0700

    Написана программа

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Lesson_2
+﻿using System;
+namespace Lesson_2
 {
     internal class Program
     {
         static void Main()
         {
-            
+            string myName;
+            myName = "Olga";
+            Console.WriteLine(myName);
+            myName = "Maria";
+            Console.WriteLine(myName);
+            Console.ReadKey();
         }
     }
 }
