eee7e0bd85a9111d760f8bf172d47fd3b17bfa67
OlgaSm33 <olya.sudakova98@gmail.com>
2024-11-21 22:02:26 +0700

    Написали кодик

--- Lesson_2/Program.cs
+++ Lesson_2/Program.cs
@@ -5,15 +5,40 @@ namespace Lesson_2
     {
         static void Main()
         {
-            const string yourName = "Lolo"; // Литерал 
-            string myName;
-            myName = "Olga";
-            Console.WriteLine(myName);
-            myName = "Maria";
-            Console.WriteLine(myName);
-            Console.WriteLine("\x23");
-            Console.WriteLine(true);
-            Console.ReadKey();
+            Console.WriteLine("Как Вас зовут?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Сколько Вам лет?");
+            int age = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Есть ли у Вас питомец?");
+            bool pet = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Какой у Вас размер ноги?");
+            double foot = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("My age is " + Convert.ToString(age));
+            Console.WriteLine("Do I have a pet? " + Convert.ToString(pet));
+            Console.WriteLine("My shoe size is " + Convert.ToString(foot));
+
         }
     }
 }
+
+/* using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string MyName = "Jane";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 37.5;
+
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
+
+    }
+}
+*/
